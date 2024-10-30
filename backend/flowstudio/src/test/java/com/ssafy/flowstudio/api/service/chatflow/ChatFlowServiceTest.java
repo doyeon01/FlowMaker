9f9ed821a68e9152daa1f9be52b14140f87caed8
JaewonChoi <wefhio1985@gmail.com>
2024-10-30 17:48:13 +0900

    save

---
+++ backend/flowstudio/src/test/java/com/ssafy/flowstudio/api/service/chatflow/ChatFlowServiceTest.java
@@ -0,0 +1,14 @@
+package com.ssafy.flowstudio.api.service.chatflow;
+
+import com.ssafy.flowstudio.support.IntegrationTestSupport;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@ActiveProfiles("test")
+class ChatFlowServiceTest extends IntegrationTestSupport {
+
+
+}
